a2bbb2641d972765def8e8148865dac213e99386
Andrey Tunyatkin <andtun@yandex.ru>
2017-11-18 17:45:04 +0300

    historic filter

--- bighack/core/models.py
+++ bighack/core/models.py
@@ -7,7 +7,6 @@ class Building(models.Model):
     address = models.CharField('адрес', max_length=1024)
     license = models.CharField('лицензия', max_length=128)
     square = models.FloatField('площадь')
-    historic = models.BooleanField('историческое значение')
     latitude = models.FloatField('широта')
     longitude = models.FloatField('долгота')
 
--- getData.py
+++ getData.py
@@ -8,6 +8,7 @@ MAIN_URL = "https://apidata.mos.ru/v1/datasets/1022/"
 DATA_URL = MAIN_URL + "rows?projection=null&api_key=%s&$top=%s&$skip=%s"
 NUMBER_URL = MAIN_URL + "count?api_key=%s"
 
+OFFER_NUM = 9000
 
 def get_data():
 
@@ -21,20 +22,20 @@ def get_data():
         number = 0
 
     # get offers by packs of 500
-    for offset in range(0, number, 500):
+    for offset in range(0, OFFER_NUM, 500):
         url = DATA_URL % (API_KEY, 500, offset)
         r = requests.get(url)
         if r.status_code == 200:
             allData = r.json()
             for d in allData:
                 data = d['Cells']
-                b = Building()
-                b.address = data['Address']
-                b.license = data['EGRPLicenceNumber']
-                b.square = data['ObjectArea']
-                b.historic = (data['HistoricalAndCulturalHeritageCategory'] is not None)
-                b.latitude = data['geoData']['coordinates'][1]
-                b.longtuide = data['geoData']['coordinates'][0]
-                b.save()
+                if data['HistoricalAndCulturalHeritageCategory'] is None:
+                    b = Building()
+                    b.address = data['Address']
+                    b.license = data['EGRPLicenceNumber']
+                    b.square = data['ObjectArea']
+                    b.latitude = data['geoData']['coordinates'][1]
+                    b.longtuide = data['geoData']['coordinates'][0]
+                    b.save()
         else:
             print("fail")
